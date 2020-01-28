3d64296ad3355df12438d6af54cc18dffe29a0d0
guanw <guanw0826@gmail.com>
2020-01-28 10:42:19 -0500

    add metrics to retry handler

--- pkg/store/retry_handler_test.go
+++ pkg/store/retry_handler_test.go
@@ -5,25 +5,37 @@ import (
 	"testing"
 
 	"github.com/guanw/ct-dns/pkg/store/mocks"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	metrics      = InitializeMetrics()
+	maximumRetry = 10
 )
 
 func TestRetryHandler_GetService(t *testing.T) {
 	mockStore := &mocks.Store{}
 	mockStore.On("GetService", "valid-service").Return([]string{"192.0.0.1:8081"}, nil)
 	mockStore.On("GetService", "error-service").Return(nil, errors.New("new error"))
-	retryHandler := NewRetryHandler(5, mockStore)
+	retryHandler := NewRetryHandler(maximumRetry, mockStore, metrics)
 	tests := []struct {
-		ExpectError bool
-		ServiceName string
+		ExpectError            bool
+		ServiceName            string
+		ExpectedRetryAttempts  float64
+		ExpectedRetryExhausted float64
 	}{
 		{
-			ServiceName: "valid-service",
-			ExpectError: false,
+			ServiceName:            "valid-service",
+			ExpectError:            false,
+			ExpectedRetryAttempts:  0.0,
+			ExpectedRetryExhausted: 0.0,
 		},
 		{
-			ServiceName: "error-service",
-			ExpectError: true,
+			ServiceName:            "error-service",
+			ExpectError:            true,
+			ExpectedRetryAttempts:  float64(maximumRetry),
+			ExpectedRetryExhausted: 1.0,
 		},
 	}
 	for _, test := range tests {
@@ -33,6 +45,8 @@ func TestRetryHandler_GetService(t *testing.T) {
 		} else {
 			assert.NoError(t, err)
 		}
+		assert.Equal(t, test.ExpectedRetryAttempts, testutil.ToFloat64(metrics.GetServiceRetryAttempts))
+		assert.Equal(t, test.ExpectedRetryExhausted, testutil.ToFloat64(metrics.GetServiceRetryExhausted))
 	}
 }
 
@@ -40,24 +54,30 @@ func TestRetryHandler_PostService(t *testing.T) {
 	mockStore := &mocks.Store{}
 	mockStore.On("UpdateService", "service", "add", "192.0.0.1:8081").Return(nil)
 	mockStore.On("UpdateService", "service", "invalid-operation", "xxx").Return(errors.New("new error"))
-	retryHandler := NewRetryHandler(5, mockStore)
+	retryHandler := NewRetryHandler(maximumRetry, mockStore, metrics)
 	tests := []struct {
-		ExpectError bool
-		ServiceName string
-		Operation   string
-		Host        string
+		ExpectError            bool
+		ServiceName            string
+		Operation              string
+		Host                   string
+		ExpectedRetryExhausted float64
+		ExpectedRetryAttempts  float64
 	}{
 		{
-			ServiceName: "service",
-			Operation:   "add",
-			Host:        "192.0.0.1:8081",
-			ExpectError: false,
+			ServiceName:            "service",
+			Operation:              "add",
+			Host:                   "192.0.0.1:8081",
+			ExpectError:            false,
+			ExpectedRetryExhausted: 0.0,
+			ExpectedRetryAttempts:  0.0,
 		},
 		{
-			ServiceName: "service",
-			Operation:   "invalid-operation",
-			Host:        "xxx",
-			ExpectError: true,
+			ServiceName:            "service",
+			Operation:              "invalid-operation",
+			Host:                   "xxx",
+			ExpectError:            true,
+			ExpectedRetryExhausted: 1.0,
+			ExpectedRetryAttempts:  float64(maximumRetry),
 		},
 	}
 	for _, test := range tests {
@@ -67,5 +87,7 @@ func TestRetryHandler_PostService(t *testing.T) {
 		} else {
 			assert.NoError(t, err)
 		}
+		assert.Equal(t, test.ExpectedRetryAttempts, testutil.ToFloat64(metrics.PostServiceRetryAttempts))
+		assert.Equal(t, test.ExpectedRetryExhausted, testutil.ToFloat64(metrics.PostServiceRetryExhausted))
 	}
 }
